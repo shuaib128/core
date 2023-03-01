f830f5387a11341e0c91f3d1da6bf43d30df2a5c
shuaib128 <shuaibahamed77@gmail.com>
2023-03-01 00:21:27 -0800

    added .env

--- core/settings.py
+++ core/settings.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w-ux^eb9@3*a93l$d_5$l*u1+u&ws*0my9fz_1(0h+*pzf*0$k'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -79,7 +80,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get("REDIS_URL", "redis://default:IBVhmzoCOZVoDLaOSul9@containers-us-west-92.railway.app:5903")],
+            "hosts": [os.getenv("REDIS_URL")],
         },
     },
 }
