c55c9159fb2d7d1798d936a07ee30608a6d7e60f
sami <admin@seattledevicerepair.com>
2023-03-23 00:26:17 -0700

    removed md

--- core/asgi.py
+++ core/asgi.py
@@ -5,8 +5,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter(
